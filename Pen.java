0e9ecff3a041b2b039cc9812cf215fb75aa0bc53
tibame13 <istyle1105@gmail.com>
2020-03-14 14:05:31 +0800

    second

---
+++ Pen.java
@@ -0,0 +1,35 @@
+package HW1217;
+
+public abstract class Pen implements IWritable {
+	protected String brand;
+	protected int price;
+	
+	//Constructor
+	public Pen() {
+	}
+	
+	//Constructor
+	public Pen(String brand,int price) {
+		this.brand=brand;
+		this.price=price;
+	}
+	
+	public void SetBrand(String brand) {
+		this.brand=brand;
+	}
+	
+	public void SetPrice(int price) {
+		this.price=price;
+	}
+	
+	public String GetBrand() {
+		return brand;
+	}
+	
+	public int GetPrice() {
+		return price;
+	}
+	
+	public abstract void write();
+		
+}
